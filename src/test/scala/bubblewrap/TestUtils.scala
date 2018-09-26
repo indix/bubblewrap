27f6cbfd71a6e1c6237689bbd27099501b990b72
GP <ganeshp@indix.com>
2018-09-26 16:00:14 +0530

    Remove encoding parameter on readAsBytes() method.
    Encoding doesn't make sense for byte arrays!

--- src/test/scala/bubblewrap/TestUtils.scala
+++ src/test/scala/bubblewrap/TestUtils.scala
@@ -8,5 +8,5 @@ import scala.concurrent.{Await, Future}
 object TestUtils {
   def get[T](future:Future[T], duration:FiniteDuration = 100 millis): T = Await.result(future, duration)
   def readAsString(resource: String, encoding: String = "UTF-8"): String = IOUtils.toString(getClass.getResourceAsStream(resource),encoding)
-  def readAsBytes(resource: String, encoding: String = "UTF-8") = IOUtils.toByteArray(getClass.getResourceAsStream(resource))
+  def readAsBytes(resource: String): Array[Byte] = IOUtils.toByteArray(getClass.getResourceAsStream(resource))
 }
