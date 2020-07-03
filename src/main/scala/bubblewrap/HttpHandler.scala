119e8da9ede0f5616877105a1df6ae913b82f90d
Anant Khaitan <anant.khaitan@avalara.com>
2020-07-03 11:13:12 +0530

    Log error message and stacktrace

--- src/main/scala/bubblewrap/HttpHandler.scala
+++ src/main/scala/bubblewrap/HttpHandler.scala
@@ -7,12 +7,15 @@ import java.util.concurrent.TimeoutException
 import io.netty.handler.codec.http.{DefaultHttpHeaders, HttpHeaders => HttpResponseHeaders}
 import org.asynchttpclient.AsyncHandler.State
 import org.asynchttpclient.{AsyncHandler, HttpResponseBodyPart, HttpResponseStatus}
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.Promise
 
-class HttpHandler(config:CrawlConfig, url: WebUrl, var httpResponse: Promise[HttpResponse]) extends AsyncHandler[Unit]{
+class HttpHandler(config: CrawlConfig, url: WebUrl, var httpResponse: Promise[HttpResponse]) extends AsyncHandler[Unit] {
+  val logger = LoggerFactory.getLogger(classOf[HttpHandler])
+
   private[bubblewrap] var body = new ResponseBody
-  var statusCode:Int = 200
+  var statusCode: Int = 200
   var headers: ResponseHeaders = new ResponseHeaders(new DefaultHttpHeaders)
   val startTime = System.currentTimeMillis()
   val UNKNOWN_ERROR = 1006
@@ -21,12 +24,15 @@ class HttpHandler(config:CrawlConfig, url: WebUrl, var httpResponse: Promise[Htt
   val CAPTCHA_CONTENT = 9997
 
   private def errorCode(error: Throwable) = error match {
-    case timeout:TimeoutException => TIMEOUT_ERROR
-    case other:IOException if other.getMessage.contains("Remotely closed") => REMOTE_CLOSE
+    case timeout: TimeoutException => TIMEOUT_ERROR
+    case other: IOException if other.getMessage.contains("Remotely closed") => REMOTE_CLOSE
     case other => UNKNOWN_ERROR
   }
 
-  override def onThrowable(error: Throwable) = httpResponse.success(HttpResponse(errorCode(error), FailureResponse(error), headers, responseTime))
+  override def onThrowable(error: Throwable) = {
+    logger.warn(s"[BubbleWrap] Fetch Failed for url=$url, crawlConfig=$config", error)
+    httpResponse.success(HttpResponse(errorCode(error), FailureResponse(error), headers, responseTime))
+  }
 
 
   override def onCompleted(): Unit = {
@@ -39,7 +45,7 @@ class HttpHandler(config:CrawlConfig, url: WebUrl, var httpResponse: Promise[Htt
   override def onBodyPartReceived(bodyPart: HttpResponseBodyPart): State = {
     val part = bodyPart.getBodyPartBytes
     body.write(part)
-    if (body.exceedsSize(config.maxSize)){
+    if (body.exceedsSize(config.maxSize)) {
       httpResponse.success(HttpResponse(statusCode, FailureResponse(ExceededSize(config.maxSize)), headers, responseTime))
       body = null
       return State.ABORT
@@ -54,7 +60,7 @@ class HttpHandler(config:CrawlConfig, url: WebUrl, var httpResponse: Promise[Htt
 
   override def onHeadersReceived(httpResponseHeaders: HttpResponseHeaders): State = {
     headers = new ResponseHeaders(httpResponseHeaders)
-    if(headers.exceedsSize(config.maxSize)) {
+    if (headers.exceedsSize(config.maxSize)) {
       httpResponse.success(HttpResponse(statusCode, FailureResponse(ExceededSize(config.maxSize)), headers, responseTime))
       return State.ABORT
     }
